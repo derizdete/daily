ae022bbcdf65f700e29c600ed980d4387c427465
derizdete <haidaeraf@gmail.com>
2026-01-13 12:53:57 +0700

    add task toggle and dark mode
    Fix typos and improve event listener functionality for item clicks and theme toggling.

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
-const items = document.querySelectrAll("li");
+const items =
+  document.querySelectorAll("#schedule li");
+const toggleBtn =
+  document.getElementById("toggleTheme");
 
-items/forEach(item =>{
-  item.addEeventListener("click",() => {
+items.forEach((item,index) => {
+  if(localStorage.getItem("task-"+ index)==="done"){
+    item.classList.add("done");
+  }
+  item.addEventListener("click", () => {
     item.classList.toggle("done");
+
+    if(item.classList.contains("done")) {
+      localStorage.setItem("task-" + index, "done");
+    } else{
+      localStorage.removeItem("task-" + index);
+    }
   });
+});
+
+if(localStorage.getItem("theme") === "dark"){
+  document.body.vlassList.add("darl");
+  toggleBtn.textContent = "Light Mode";
+}
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+  if(document.body.classlIst.contains("dark"))
+  {
+    localStorage.setItem("theme", "dark");
+    toggleBtn.textContent = "Light Mode";
+  } else {
+    localStorage.setItem("theme", "light");
+    toggleBtn.textContent = "Dark Mode";
+  }
 });
